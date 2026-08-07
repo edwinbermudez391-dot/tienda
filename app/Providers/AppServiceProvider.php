14d3e11fad5e93b4f6d417dc98778cbd5dc2fa9b
Edwin <edwinbermudez391@gmail.com>
2026-08-07 15:14:40 -0500

    Agrega forceScheme para HTTPS en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. Añade esta línea arriba
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Agrega este bloque dentro del método boot
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
