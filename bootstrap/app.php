d74f102e0310db045624c931b642b69dd6fa2b5f
Edwin <edwinbermudez391@gmail.com>
2026-08-10 21:08:19 -0500

    error 411 solucion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\TokenMismatchException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (TokenMismatchException $e, $request) {
+            return redirect()->back()
+                ->withInput($request->except('password', '_token'))
+                ->with('error', 'Por seguridad, tu sesión ha expirado por inactividad. Por favor, inténtalo de nuevo.');
+        });
     })->create();
